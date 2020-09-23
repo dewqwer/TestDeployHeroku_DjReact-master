b132ee4849678e080e8a607f38d4dbe87ce2b7e5
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-23 13:18:08 +0700

    ลองเปลี่ยน path urls.py

--- djreact/urls.py
+++ djreact/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include, re_path, url
 from django.views.generic import TemplateView
+from articles.api import urls as api_urls
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('api/', include('articles.api.urls')),
-    path('admin/', admin.site.urls),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('rest-auth/', include('rest_auth.urls')),
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # path('api/', include('articles.api.urls')),
+    # path('admin/', admin.site.urls),
+    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(api_urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
